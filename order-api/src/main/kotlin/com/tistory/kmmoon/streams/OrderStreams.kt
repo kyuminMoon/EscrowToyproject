755448b7f6d1c96e1359af07d9672c798b341b4f
kyuminMoon <nemesis1001@kakao.com>
2023-07-08 23:22:03 +0900

    ✨Feat - 결제 관련 이벤트 생성(stream 관련 공부중...)

--- order-api/src/main/kotlin/com/tistory/kmmoon/streams/OrderStreams.kt
+++ order-api/src/main/kotlin/com/tistory/kmmoon/streams/OrderStreams.kt
@@ -41,7 +41,10 @@ import java.util.function.Supplier
 
 /**
  * 위 2개의 코드는 동일하게 동작하나, Deprecate 되어 찾아본 결과 yml을 사용한 방식으로 변경됐다고 함.
+ * @link https://docs.spring.io/spring-cloud-stream/docs/Brooklyn.RELEASE/reference/htmlsingle/
  * @link https://stackoverflow.com/questions/65978055/enablebinding-output-input-deprecated-since-version-of-3-1-of-spring-cloud-str
+ * @link https://kamang-it.tistory.com/entry/KafkaJavaSpringBootSpringCloudStreambinder%EC%99%80-bindings-output-bind-2
+ *
  * */
 @Service
 class OrderStreams {
